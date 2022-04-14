ca384c8c99733577fc93a4a2fcb45464c527b607
unknown <ankit.singh607.com>
2022-04-13 17:48:50 -1200

    practice day

--- beginners/azure/aks_cluster/main.tf
+++ beginners/azure/aks_cluster/main.tf
@@ -7,7 +7,7 @@
 #Add Azure Provider
 provider "azurerm" {
 
-    version         =   "~>2.0"
+    version         =   "~>2.1"
     client_id       =   var.client_id
     client_secret   =   var.client_secret
     subscription_id =   var.subscription_id
